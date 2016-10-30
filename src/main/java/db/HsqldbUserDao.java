e27a8253fff522e13d4ca7d585179bf785e5888c
DESKTOP-SLH2AC3\Zend <k.g.prihodko@gmail.com>
2016-10-30 20:09:47 +0200

    Create a database via DatabaseManager and successfully tested a method <Create>

--- src/main/java/db/HsqldbUserDao.java
+++ src/main/java/db/HsqldbUserDao.java
@@ -3,6 +3,12 @@ package db;
 import java.util.Collection;
 
 import java.lang.Exception;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import kn145.prihodko.usermanagement.User;
 
@@ -10,6 +16,7 @@ import org.dbunit.DatabaseTestCase;
 
 public class HsqldbUserDao implements UserDAO {
 
+	private static final String INSERT_QUERY = "INSERT INTO users (firstname, lastname, dateofbirth) VALUES (?, ?, ?)";
 	private ConnectionFactory connectionFactory;
 	
 	public HsqldbUserDao(ConnectionFactory connectionFactory){
@@ -17,8 +24,31 @@ public class HsqldbUserDao implements UserDAO {
 	}
 	@Override
 	public User create(User user) throws DatabaseException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Connection connection = connectionFactory.createConnection();
+			PreparedStatement statement = connection.prepareStatement(INSERT_QUERY);
+			statement.setString(1, user.getFirstName());
+			statement.setString(2, user.getLastName());
+			statement.setDate(3, new Date(user.getDateOfBirthd().getTime()));
+			int n = statement.executeUpdate();
+			if(n != 1) {
+				throw new DatabaseException("The number of the inserted rows is " + n);
+			}
+			CallableStatement callableStatement = connection.prepareCall("call IDENTITY()");
+			ResultSet keys = callableStatement.executeQuery();
+			if(keys.next()) {
+				user.setId(new Long(keys.getLong(1)));
+			}
+			keys.close();
+			callableStatement.close();
+			statement.close();
+			connection.close();
+			return user;
+		} catch (SQLException e) {
+			throw new DatabaseException();
+		} catch (DatabaseException e) {
+			throw e;
+		}
 	}
 
 	@Override
